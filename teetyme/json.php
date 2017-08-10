a170f9330661a67237be708b8150b94e1c9118cd
aaronhughey <30471014+aaronhughey@users.noreply.github.com>
2017-08-09 23:27:45 -0400

    Update json.php

--- teetyme/json.php
+++ teetyme/json.php
@@ -1,10 +1,22 @@
 <?php
   include "../crud2/database.php";
-  echo json_encode(
-    Database::prepare(
-      'SELECT * FROM `tt_courses`',
-       array()
-    )->fetchAll(PDO::FETCH_ASSOC)
-  );
+  
+  if(isset($_GET['id'])){
+	  
+	echo json_encode(
+		Database::prepare(
+			'SELECT * FROM `tt_courses` WHERE id=' . $_GET['id'],
+			array()
+		)->fetchAll(PDO::FETCH_ASSOC)
+	);
+  }
+  else
+	echo json_encode(
+		Database::prepare(
+			'SELECT * FROM `tt_courses`',
+			array()
+		)->fetchAll(PDO::FETCH_ASSOC)
+	);	  
 ?>
+
 
